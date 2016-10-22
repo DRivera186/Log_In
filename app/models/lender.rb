d94a08ad71e7a4f8e750a7e270d2d67c0e871edb
Daniel Riverae <D.Rivera186@GMail.com>
2016-10-22 01:08:30 -0700

    bug fixes
    bugs

--- app/models/lender.rb
+++ app/models/lender.rb
@@ -2,7 +2,7 @@ class Lender < ActiveRecord::Base
   has_secure_password
   has_many :histories
   
-  validates :first_name, :last_name,  :presence => true
+  validates :first_name, :last_name, :money,  :presence => true
   validates :money,  numericality: { only_integer: true }
   email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
  	has_many :histories
   # validates :password, :presence => true, :on => :create
-  validates :first_name, :last_name, :purpose, :description,  :presence => true
+  validates :first_name, :last_name, :purpose, :description, :money,  :presence => true
   validates :money,  numericality: { only_integer: true }
   email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :email, :presence => true, :format => { :with => email_regex }, :uniqueness => true
